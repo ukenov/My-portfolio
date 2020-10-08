252f482acef0089d228fc7384e901e603565b245
Asylhan Ukenov <ukenov.asylhan@gmail.com>
2020-10-08 01:00:08 -0400

    Update all the files

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,11 +1,14 @@
-// IIFE -- Immediately Invoked Function Expression
-(function(){
+/* app.js, Asylhan Ukenov, 301108165, 07-10-2020*/
+/* Call function after form submit button clicked to store information in variables */
+var button = document.getElementById('submitBtn');
 
-    function Start()
-    {
-        console.log("App Started...");
-    }
+button.onclick = function() {
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let phoneNumber = document.getElementById("phoneNumber").value;
+    let emailAddress = document.getElementById("emailAddress").value;
+    let shortMessage = document.getElementById("shortMessage").value;
+    console.log(firstName, lastName, phoneNumber, emailAddress, shortMessage);
 
-    window.addEventListener("load", Start);
-
-})();
+    location.href = '/home';
+}
--- routes/index.js
+++ routes/index.js
@@ -11,24 +11,24 @@ router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home'});
 });
 
-/* GET About Us page. */
+/* GET about me page. */
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About'});
 });
 
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects'});
 });
 
-/* GET Services page. */
+/* GET services page. */
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services'});
 });
 
-/* GET Contact Us page. */
+/* GET contact us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+  res.render('index2', { title: 'Contact'});
 });
 
 module.exports = router;
