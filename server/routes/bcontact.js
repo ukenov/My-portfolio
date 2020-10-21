75b43b55db03f3a1a0b69a3c80416a8fabf9d52d
Asylhan Ukenov <ukenov.asylhan@gmail.com>
2020-10-21 00:07:47 -0400

    Sort contacts list

--- server/routes/bcontact.js
+++ server/routes/bcontact.js
@@ -16,9 +16,70 @@ router.get('/', (req, res, next) => {
     {
         //console.log(BcontactList);
 
-        res.render('business_contacts', {title: 'Business Contacts', BcontactList: bcontactList});
+        res.render('bcontact/list', {title: 'Business Contacts', BcontactList: bcontactList});
     }
  });
 })
+
+// GET Route for displaying the Edit page - UPDATE Operation
+router.get('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    Bcontact.findById(id, (err, contactToEdit) => {
+        if(err) 
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else 
+        {
+            // show the edit view
+            res.render('bcontact/edit', {title: 'Edit Contact', contact: contactToEdit})
+        }
+    })
+});
+
+// POST Route for processing the Edit page - UPDATE Operation
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    let updatedContact = Bcontact({
+        "_id": id,
+        "name": req.body.name,
+        "number": req.body.number,
+        "email": req.body.email
+    });
+
+    Bcontact.updateOne({_id:id}, updatedContact, (err) => {
+        if(err) 
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else 
+        {
+            // refresh the business contact list
+            res.redirect('/bcontact-list');
+        }
+    });
+});
+
+// GET to perform Deletion - DELETE Operation
+router.get('/delete/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    Bcontact.remove({_id:id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else 
+        {
+            // refresh the business contact list
+            res.redirect('/bcontact-list');
+        }
+    });
+});
 
 module.exports = router;
